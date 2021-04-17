6191639211239a8eee780862e0186652256be221
satwikbhosale <78591335+satwikbhosale@users.noreply.github.com>
2021-04-17 14:49:38 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,27 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+   this.Visiblity=255;
+  }
+display()
+{
+  //console.log(this.body.)
+if((this.body.speed)<2.5)
+{super.display();
+}else
+{
+World.remove(world,this.body)  
+
+push();
+this.Visiblity=this.Visiblity-5;
+tint(255,this.Visiblity)
+if (this.Visiblity<0 && this.Visiblity>-100){
+  score=score+10
+}
+image(this.image,this.body.position.x,this.body.position.y,50,50)
+pop();
+}}
+};
+
+
